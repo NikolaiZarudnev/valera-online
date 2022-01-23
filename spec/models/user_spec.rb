d2282414dba623ebd5e00dbea2401986affa13cd
NikolaiZarudnev <nikolaizarudnrv@gmail.com>
2022-01-23 15:24:24 +0700

    yep

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe '#user' do
     context 'when Valera go to work' do
-      let(:valera_action) { FactoryBot.create(:valera_action) }
+      valera_action = FactoryBot.create(:valera_action)
 
       it { expect(valera_action.health).to eq 1 }
       it { expect(valera_action.money).to eq 1 }
--- spec/models/valera_action_spec.rb
+++ spec/models/valera_action_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ValeraAction, type: :model do
   describe '#action' do
     context 'with valid action' do
-      let(:valera_action) { FactoryBot.create(:valera_action) }
+      valera_action = FactoryBot.create(:valera_action)
 
       it { expect(valera_action).to be_valid }
     end
